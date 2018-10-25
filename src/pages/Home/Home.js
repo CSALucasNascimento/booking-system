fe92127d9958131e6ea64dbf7327bf49a8782015
Lucas Nascimento <lucasnascimento@Lucass-MacBook-Air.local>
2018-10-26 09:20:19 +1100

    adding spaces home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,12 +13,9 @@ import s from './Home.css';
 
 import { Button } from 'reactstrap';
 import { Analytics, API, graphqlOperation } from "aws-amplify";
+import { Connect } from 'aws-amplify-react';
 import * as queries from '../../graphql/queries';
 
-
-// Simple query
-const listSpaces = await API.graphql(graphqlOperation(queries.listSpaces));
-console.log(listSpaces.json());
 
 class Home extends React.Component {
 
@@ -31,12 +28,29 @@ class Home extends React.Component {
 
   render() {
     
+    const ListSpaces = ({ spaces }) => (
+      <div>
+        <h3>All Space</h3>
+        <ul>
+          { spaces.map( (space) => <li key={space.id} >{space.title}</li> ) }
+        </ul>
+      </div>
+    );
+
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1>React.js News</h1>
           <Button onClick={this.handleAnalytics}> Test Analytics </Button>
-          
+          <Connect query={graphqlOperation(queries.listSpaces)}>
+            {
+              ({data: {listSpaces}, loading, error}) => {
+                if (error) return (<h3>Error</h3>);
+                if (loading || !listSpaces ) return (<h3>Loading...</h3>);
+                <ListSpaces spaces={listSpaces.items} />
+              }
+            }
+          </Connect>
         </div>
       </div>
     );
